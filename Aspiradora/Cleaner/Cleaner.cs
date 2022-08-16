c62eb6f1dc2ae626ec8e9cdea4dcddf71fba203b
Lucas Agustin Neuwit Quaroni <lquaroni@itsynch.com>
2022-08-16 20:20:11 -0300

    initial commit

---
+++ Aspiradora/Cleaner/Cleaner.cs
@@ -0,0 +1,27 @@
+﻿public class Cleaner
+{
+    public Action Do(Location location, State state)
+    {
+        return state == State.Dirty ? Action.Clear : location == Location.A ? Action.MoveRight : Action.MoveLeft;
+    }
+}
+
+public enum Location
+{
+    A = 0,
+    B = 1,
+}
+
+public enum State
+{
+    Clean = 0,
+    Dirty = 1,
+}
+
+public enum Action
+{
+    Clear = 0,
+    MoveRight = 1,
+    MoveLeft = 2
+}
+
